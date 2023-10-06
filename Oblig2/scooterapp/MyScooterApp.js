8fc92abc0b2a61a1683cacf14102a916a3c4ce54
mso201uit <98842228+mso201uit@users.noreply.github.com>
2023-10-07 00:33:13 +0200

    added lightshader

--- Oblig2/scooterapp/MyScooterApp.js
+++ Oblig2/scooterapp/MyScooterApp.js
@@ -1,7 +1,7 @@
 import {BaseApp} from '../../base/BaseApp.js';
 import {Scooter} from '../scooter/scooter.js';
 import {WebGLShader} from "../../base/helpers/WebGLShader.js";
-
+import {vectorToString} from "../../base/lib/utility-functions.js";
 export class MyScooterApp extends BaseApp {
     constructor() {
         super(true);
@@ -15,6 +15,8 @@ export class MyScooterApp extends BaseApp {
         // Texture shader:
         let vertexShaderSource = document.getElementById('texture-vertex-shader').innerHTML;
         let fragmentShaderSource = document.getElementById('texture-fragment-shader').innerHTML;
+        let lightvertexShaderSource = document.getElementById('diffuse-pointlight-gourad-vertex-shader').innerHTML;
+		let lightfragmentShaderSource = document.getElementById('diffuse-pointlight-gourad-fragment-shader').innerHTML;
         // Initialiserer  & kompilerer shader-programmene;
         const glslTextureShader = new WebGLShader(this.gl, vertexShaderSource, fragmentShaderSource);
         // Samler all texture-shader-info i et JS-objekt.
@@ -31,6 +33,35 @@ export class MyScooterApp extends BaseApp {
                 modelViewMatrix: this.gl.getUniformLocation(glslTextureShader.shaderProgram, 'uModelViewMatrix'),
             },
         };
+        const glslShader = new WebGLShader(this.gl, lightvertexShaderSource, lightfragmentShaderSource);
+        this.lightShaderInfo = {
+            program: glslShader.shaderProgram,
+            attribLocations: {
+                vertexPosition: this.gl.getAttribLocation(glslShader.shaderProgram, 'aVertexPosition'),
+                vertexNormal: this.gl.getAttribLocation(glslShader.shaderProgram, 'aVertexNormal'),
+                vertexTextureCoordinate: this.gl.getAttribLocation(glslShader.shaderProgram, 'aVertexTextureCoordinate'),
+            },
+            uniformLocations: {
+                projectionMatrix: this.gl.getUniformLocation(glslShader.shaderProgram, 'uProjectionMatrix'),
+                modelViewMatrix: this.gl.getUniformLocation(glslShader.shaderProgram, 'uModelViewMatrix'),
+                modelMatrix: this.gl.getUniformLocation(glslShader.shaderProgram, 'uModelMatrix'),
+                normalMatrix: this.gl.getUniformLocation(glslShader.shaderProgram, 'uNormalMatrix'),
+    
+                lightPosition: this.gl.getUniformLocation(glslShader.shaderProgram, 'uLightPosition'),
+                ambientLightColor: this.gl.getUniformLocation(glslShader.shaderProgram, 'uAmbientLightColor'),
+                diffuseLightColor: this.gl.getUniformLocation(glslShader.shaderProgram, 'uDiffuseLightColor'),
+    
+                sampler: this.gl.getUniformLocation(glslShader.shaderProgram, 'uSampler'),
+            },
+        };
+        this.light = {
+            lightPosition: {x: 10, y:6, z:0},
+            diffuseLightColor: {r: 1, g: 0, b:0},
+            ambientLightColor: {r: 0.2, g: 1, b:0.2},
+        };
+        document.getElementById('light-position').innerHTML = vectorToString(this.light.lightPosition);
+		document.getElementById('diffuse-light-color').innerHTML = vectorToString(this.light.diffuseLightColor);
+		document.getElementById('ambient-light').innerHTML = vectorToString(this.light.ambientLightColor);
     }
 
     handleKeys(elapsed) {
