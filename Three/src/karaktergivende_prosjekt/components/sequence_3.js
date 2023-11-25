7564bc0256d938aa20b6a8bb01fddfde6d6a3f65
Hoikka <hoikka94@hotmail.com>
2023-11-25 14:24:29 +0100

    work on swing

--- Three/src/karaktergivende_prosjekt/components/sequence_3.js
+++ Three/src/karaktergivende_prosjekt/components/sequence_3.js
@@ -29,13 +29,51 @@ export class Swing {
         const plankDepth = 10;
         const radius = 10;
         const height = 200;
-
+    
         let material = new THREE.MeshStandardMaterial({ color: 0x00ff00, side: THREE.DoubleSide });
-
+    
         let plankRigidBody = this.createPlank(material, position, plankWidth, plankLength, plankDepth, radius);
         let baseRigidBody = this.createBase(material, position, radius, height);
+    
+        // Plank pivot at one end of the plank (edge of its length)
+        const plankPivot = new Ammo.btVector3(0, 0, plankLength / 2);
+    
+        // Base pivot at the top center of the cylinder
+        const basePivot = new Ammo.btVector3(0, height / 2, 0);
+    
+        const hingeConstraint = new Ammo.btHingeConstraint(
+            baseRigidBody,
+            plankRigidBody,
+            basePivot,
+            plankPivot,
+            new Ammo.btVector3(0, 1, 0), // Axis along Y-axis
+            new Ammo.btVector3(0, 1, 0), // Same for both
+            false
+        );
+    
+        const lowerLimit = -Math.PI / 2;
+        const upperLimit = Math.PI / 2;
+        hingeConstraint.setLimit(lowerLimit, upperLimit, 0.9, 0.3, 1.0);
+        hingeConstraint.enableAngularMotor(true, 0, 0.5);
+        phy.ammoPhysicsWorld.addConstraint(hingeConstraint, false);
+    }
+    
 
-        // Legger til physics world:
+    createPlank(material, position, plankWidth, plankLength, plankDepth, radius) {
+        const plankMass = 10; // A non-zero mass makes the plank dynamic
+    
+        // Creating the plank mesh
+        const plankGeometry = new THREE.BoxGeometry(plankLength, plankWidth, plankDepth);
+        const plankMesh = new THREE.Mesh(plankGeometry, material);
+        plankMesh.position.set(position.x, position.y + radius, position.z);
+        plankMesh.rotation.set(Math.PI / 2, 0, 10 * Math.PI / 180);
+    
+        // Creating Ammo.js rigid body for the plank
+        const shape = new Ammo.btBoxShape(new Ammo.btVector3(plankLength / 2, plankWidth / 2, plankDepth / 2));
+        const plankRigidBody = createAmmoRigidBody(shape, plankMesh, 0.4, 0.6, plankMesh.position, plankMass);
+        plankRigidBody.setDamping(0.1, 0.5);
+        plankMesh.userData.physicsBody = plankRigidBody;
+
         phy.ammoPhysicsWorld.addRigidBody(
             plankRigidBody,
             COLLISION_GROUP_MOVEABLE,
@@ -45,6 +83,28 @@ export class Swing {
                 COLLISION_GROUP_PLANE |
                 COLLISION_GROUP_SPRING
         );
+
+        addMeshToScene(plankMesh);
+        phy.rigidBodies.push(plankMesh);
+        plankRigidBody.threeMesh = plankMesh;
+
+        return plankRigidBody;
+    }
+    
+    createBase(material, position, radius, height) {
+        const baseMass = 0; // Zero mass for static objects
+    
+        // Creating the cylinder mesh
+        const cylinderGeometry = new THREE.CylinderGeometry(radius, radius, height/2, 32);
+        const cylinderMesh = new THREE.Mesh(cylinderGeometry, material);
+        cylinderMesh.position.set(position.x, position.y + radius, position.z);
+        cylinderMesh.rotation.set(Math.PI / 2, 0, 10 * Math.PI / 180);
+    
+        // Creating Ammo.js rigid body for the cylinder
+        const shape = new Ammo.btCylinderShape(new Ammo.btVector3(radius, height / 2, radius));
+        shape.setMargin(0.05);
+        const baseRigidBody = createAmmoRigidBody(shape, cylinderMesh, 0.4, 0.6, cylinderMesh.position, baseMass);
+        cylinderMesh.userData.physicsBody = baseRigidBody;
 
         phy.ammoPhysicsWorld.addRigidBody(
             baseRigidBody,
@@ -56,50 +116,10 @@ export class Swing {
                 COLLISION_GROUP_SPRING
         );
 
-        //Pivotpunkt: rigidBodyCube "koples til" nedre, venstre, ytre hjørne av rigidBodyAnchor
-        let plankPivot = new Ammo.btVector3( -plankWidth/2, -plankLength/2, plankDepth/2 );
-        let basePivot = new Ammo.btVector3( -radius/2, height/2, plankDepth/2 );
-
-        let p2pConstraint = new Ammo.btPoint2PointConstraint( baseRigidBody, plankRigidBody, plankPivot, basePivot);
-        phy.ammoPhysicsWorld.addConstraint( p2pConstraint, false );
-    }
-
-    createPlank(material, position, plankWidth, plankLength, plankDepth, radius) {
-        const plankMass = 10; // A non-zero mass makes the plank dynamic
-    
-        // Creating the plank mesh
-        const plankGeometry = new THREE.BoxGeometry(plankLength, plankWidth, plankDepth);
-        const plankMesh = new THREE.Mesh(plankGeometry, material);
-        plankMesh.position.set(position.x, position.y + radius*2, position.z);
-        plankMesh.rotation.set(Math.PI / 2, 0, 10 * Math.PI / 180);
-    
-        // Creating Ammo.js rigid body for the plank
-        const shape = new Ammo.btBoxShape(new Ammo.btVector3(plankLength / 2, plankWidth / 2, plankDepth / 2));
-        const plankRigidBody = createAmmoRigidBody(shape, plankMesh, 0.4, 0.6, plankMesh.position, plankMass);
-        plankMesh.userData.physicsBody = plankRigidBody;
-
-        addMeshToScene(plankMesh);
-    
-        return plankRigidBody;
-    }
-    
-    createBase(material, position, radius, height) {
-        const baseMass = 0; // Zero mass for static objects
-    
-        // Creating the cylinder mesh
-        const cylinderGeometry = new THREE.CylinderGeometry(radius, radius, height, 32);
-        const cylinderMesh = new THREE.Mesh(cylinderGeometry, material);
-        cylinderMesh.position.set(position.x, position.y + radius, position.z);
-        cylinderMesh.rotation.set(Math.PI / 2, 0, 10 * Math.PI / 180);
-    
-        // Creating Ammo.js rigid body for the cylinder
-        const shape = new Ammo.btCylinderShape(new Ammo.btVector3(radius, height / 2, radius));
-        const baseRigidBody = createAmmoRigidBody(shape, cylinderMesh, 0.4, 0.6, cylinderMesh.position, baseMass);
-        cylinderMesh.userData.physicsBody = baseRigidBody;
-
         addMeshToScene(cylinderMesh);
+        phy.rigidBodies.push(cylinderMesh);
+        baseRigidBody.threeMesh = cylinderMesh;
     
         return baseRigidBody;
     }
-    
 }
